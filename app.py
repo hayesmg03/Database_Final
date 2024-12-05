fc040fabc983a299d508d1f6a7a6c2dd3a723f0d
Mitchell <hayesmg03@gmail.com>
2024-12-04 23:18:33 -0500

    can search for table with text input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -10,14 +10,23 @@ def get_db_connection():
     return conn
 
 # Route to display data from the database
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     conn = get_db_connection()
-    data = conn.execute('SELECT * FROM VideoGame').fetchall()  # Replace 'your_table' with your actual table name
+    data = conn.execute('SELECT * FROM Developer').fetchall()  
     conn.close()
     return render_template('index.html', data=data)
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    conn = get_db_connection()
+    if request.method == 'POST':
+        text_input = request.form['text_input']
+        # Process the text input here
+        data = conn.execute('SELECT * FROM ' + text_input)
+        return render_template('index.html', data=data)
 
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
